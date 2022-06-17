284c75091d7858137c4ed722abdeb309049be69b
edy93 <sisilrenery@gmail.com>
2022-06-17 16:35:00 +0700

    two commit

--- produk.php
+++ produk.php
@@ -1,11 +1,14 @@
 <?php
 
 class Produk {
-    
+    public $judul = "judul",
+           $pengarang = "Pengarang",
+           $penerbit = "penerbit",
+           $harga = 0;
 }
 
 $produk1 = new Produk();
+var_dump($produk1);
 
-echo "wewe";
 ?>
     
